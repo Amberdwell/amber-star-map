26673173e8a1ca2a114651e9b31118de0d49f54e
Amberdwell <info@amberdwell.com>
2026-05-20 15:32:21 +0300

    Refactor map initialization and CSV parsing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSSzkCeYF5iB99O
 
 const map = L.map('map', {
     center: [56.50, 18.00], // Centrs ap Rīgu/Latviju
-    zoom: 5,
+    zoom: 4,
     zoomControl: false,
     minZoom: 2,
     maxZoom: 20
