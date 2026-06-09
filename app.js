2fd98914fa33a4f043738a4e7f041db4ba657274
Amberdwell <info@amberdwell.com>
2026-06-09 10:53:49 +0300

    Refactor map initialization and add new features

--- app.js
+++ app.js
@@ -8,13 +8,9 @@ const map = L.map('map', {
     zoom: isMobile ? 4.2 : 5.5,
     zoomControl: false,
     minZoom: 2,
-    maxZoom: 21,
-    fullscreenControl: {
-        position: 'topright',
-        title: 'View Fullscreen',
-        titleCancel: 'Exit Fullscreen'
-    }
+    maxZoom: 21
 });
+
 L.control.zoom({ position: 'bottomright' }).addTo(map);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
